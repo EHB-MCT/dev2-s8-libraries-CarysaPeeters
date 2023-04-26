c583211a665bd4bcdb9c6f4e75134108f5dd0655
Carysa Peeters <carysa.peeters@student.ehb.be>
2023-04-26 11:30:35 +0200

    toiletten ingeladen

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ const app = {
 	map: null, // gebruik dit om de map gemakkelijk aan te spreken doorheen de applicatie
 	init() {
 		// initialise de kaart
-		var map = L.map("map").setView([50.8459299, 4.3553559], 13);
+		var map = L.map("map").setView([50.8455074, 4.3545161], 11);
 
 		// voeg een tile layer toe, met URL https://a.tile.openstreetmap.org/{z}/{x}/{y}.png
 		L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -17,6 +17,18 @@ const app = {
 
 		// gebruik de functie "loadMarkers" om de markers toe te voegen
 		L.marker([50.842239, 4.322808]).addTo(map).bindPopup("<strong>Erasmushogeschool campus Kaai</strong><br><em> MCT, TI, Musical</em>").openPopup();
+
+		fetch("https://opendata.brussels.be/api/records/1.0/search/?dataset=toiletten&q=&rows=100&geofilter.distance=50.846475%2C+4.352793%2C+5000")
+			.then(function (response) {
+				return response.json();
+			})
+			.then(function (data) {
+				console.log(data);
+				data.records.forEach(function (element) {
+					console.log(element);
+					L.marker([element.geometry.coordinates[1], element.geometry.coordinates[0]]).addTo(map).bindPopup(`${element.fields.adrvoisnl}`).openPopup();
+				});
+			});
 	},
 	loadMarkers() {
 		// fetch de data van opendata.brussels.be
